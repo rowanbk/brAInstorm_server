89e7933315b43cb2d281473077594276ca60019a
rowanbk <bk@rowan.engineer>
2020-02-03 12:42:47 -0800

    add ML support

---
+++ download_stanfordnlp.sh
@@ -0,0 +1,2 @@
+wget https://nlp.stanford.edu/software/stanford-corenlp-full-2018-10-05.zip
+unzip stanford-corenlp-full-2018-10-05.zip
---
+++ run_model.py
@@ -0,0 +1,16 @@
+import subprocess
+
+def get_sentiment(blob):
+    with open('./blob.txt','w') as f:
+        f.write(blob)
+    args = ['java','-cp','./stanford-corenlp-full-2018-10-05/*','edu.stanford.nlp.sentiment.SentimentPipeline','-file','blob.txt']
+    process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = process.communicate()
+    out = out.decode("utf-8")
+    out_sentiment = out.rstrip().rsplit('\n',1)[-1].lstrip()
+    if out_sentiment == 'Positive':
+        return 1
+    else:
+        return 0
+
+print(get_sentiment("good good good blob"))
