f8cb589f7a957c1962e8b20c9a8a0a6d5df832c6
Brandon Zhou-Charles <brandonzhoucharles@gmail.com>
2020-02-03 16:42:15 -0800

    finished post request handling

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,29 +1,30 @@
 from datetime import datetime
-from flask import Flask, render_template, jsonify
-import get_sentiment from run_model
+from flask import Flask, render_template, jsonify, request
+from flask_cors import cross_origin
 from . import app
+import subprocess
 
 @app.route("/")
-def home():
+def get_link():
     return jsonify({"response": "Hello from CORS Flask App"})
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+@app.route("/api/get_sentiment", methods=['POST'])
+@cross_origin(allow_headers=['Content-Type'])
+def get_sentiment():
+  req_data = request.get_json()
 
-@app.route("/contact/")
-def contact():
-    return render_template("contact.html")
+  blob = req_data['blob']
+  with open('./blob.txt','w') as f:
+    f.write(blob)
+  args = ['java','-cp','./stanford-corenlp-full-2018-10-05/*','edu.stanford.nlp.sentiment.SentimentPipeline','-file','blob.txt']
+  process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  out, err = process.communicate()
+  out = out.decode("utf-8")
+  out_sentiment = out.rstrip().rsplit('\n',1)[-1].lstrip()
+  sentiment = 0
+  if out_sentiment == 'Positive':
+      sentiment = 1
+  else:
+      sentiment = 0
 
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello_there(name = None):
-    return render_template(
-        "hello_there.html",
-        name=name,
-        date=datetime.now()
-    )
-
-@app.route("/api/data")
-def get_data():
-    return app.send_static_file("data.json")
+  return jsonify({"sentiment": sentiment})
