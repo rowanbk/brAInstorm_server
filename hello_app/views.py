a7b0289a3e6b7cc1d00aca8d46ef6671f14f9a56
rowanbk <bk@rowan.engineer>
2020-02-03 18:46:57 -0800

    test commit

--- hello_app/views.py
+++ hello_app/views.py
@@ -12,15 +12,15 @@ def get_link():
 @cross_origin(allow_headers=['Content-Type'])
 def get_sentiment():
   req_data = request.get_json()
-
-  blob = req_data['blob']
-  with open('./blob.txt','w') as f:
-    f.write(blob)
-  args = ['java','-cp','./stanford-corenlp-full-2018-10-05/*','edu.stanford.nlp.sentiment.SentimentPipeline','-file','blob.txt']
-  process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  out, err = process.communicate()
-  out = out.decode("utf-8")
-  out_sentiment = out.rstrip().rsplit('\n',1)[-1].lstrip()
+  print(req_data)
+  #blob = req_data['blob']
+  #with open('./blob.txt','w') as f:
+    #f.write(blob)
+  #args = ['java','-cp','./stanford-corenlp-full-2018-10-05/*','edu.stanford.nlp.sentiment.SentimentPipeline','-file','blob.txt']
+  #process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  #out, err = process.communicate()
+  #out = out.decode("utf-8")
+  #out_sentiment = out.rstrip().rsplit('\n',1)[-1].lstrip()
   sentiment = 0
   if out_sentiment == 'Positive':
       sentiment = 1
